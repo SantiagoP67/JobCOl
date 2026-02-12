2ec83c4a5945e00f00eb682cf90e589d73cc5d77
SantiagoP67 <challengeboys399@gmail.com>
2026-02-12 17:44:57 -0500

    first commit

---
+++ gateway-server/src/test/java/com/jobcol/gateway_server/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobcol.gateway_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
